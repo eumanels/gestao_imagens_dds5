9e14b670d1829b59f0a049bcb018d3fd66b8bb38
EMANOEL VITOR VENTURA ATANAZIO <e.atanazio@labcetec.net>
2024-10-30 17:17:59 -0300

    Upload de imagens

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import fileUpload from 'express-fileupload';
+import {criarImagem, mostrarImagem} from './controllers/ImagemController.js'
+const app = express();
+const porta = 5000;
+
+app.get('/', (req, res)=>{
+    res.send('API Funcionando');
+});
+
+app.use(fileUpload());
+
+app.get('/public/:nomeImg', mostrarImagem);
+//CRUD Imagem
+app.post('/imagem', criarImagem);
+
+app.listen(porta, ()=>{
+    console.log(`API Rodando na porta ${porta}`)
+});
