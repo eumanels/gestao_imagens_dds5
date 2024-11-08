7e79f228aa309a5e0f76fc511d9eabce58ee2a16
EMANOEL VITOR VENTURA ATANAZIO <e.atanazio@labcetec.net>
2024-11-08 17:07:46 -0300

    adicionando validação de login no back e no front

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import fileUpload from 'express-fileupload';
 import {criarImagem, deletarImagem, editarImagem, downloadImagem, mostrarImagens, mostrarUmaImagem} from './controllers/ImagemController.js';
 import cors from 'cors';
+import { criarUsuario, logarUsuario, mostrarUmUsuario, mostrarUsuario } from './controllers/UsuarioController.js';
 const app = express();
 const porta = 5000;
 
@@ -20,6 +21,14 @@ app.get('/imagem', mostrarImagens);
 app.get('/imagem/:id_imagem', mostrarUmaImagem);
 app.put('/imagem/:id_imagem', editarImagem);
 app.delete('/imagem/:id_imagem', deletarImagem);
+
+//CRUD USUARIO
+app.post('/usuario', criarUsuario);
+app.get('/usuario', mostrarUsuario);
+app.get('/usuario/:id_usuario', mostrarUmUsuario);
+
+//Efetuar Login
+app.post('/login', logarUsuario);
 
 app.listen(porta, ()=>{
     console.log(`API Rodando na porta ${porta}`)
